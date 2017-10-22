125022fc3e108d93e8f8a842ec4fb2883fcf08c0
Peter Kolski <peter@bildpeter.de>
2017-10-22 23:34:18 +0200

    Main project file

---
+++ main/MauAr/MauAr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MauAr
+//
+//  Created by Peter A. Kolski on 22.10.17.
+//  Copyright © 2017 Peter A. Kolski. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
