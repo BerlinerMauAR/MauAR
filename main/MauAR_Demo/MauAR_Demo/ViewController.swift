632779bd9f8b4bce79facac24113a24dc9c3e883
Peter Kolski <peter@bildpeter.de>
2017-11-23 12:24:09 +0100

    Simply showing  wall model

--- main/MauAR_Demo/MauAR_Demo/ViewController.swift
+++ main/MauAR_Demo/MauAR_Demo/ViewController.swift
@@ -10,37 +10,50 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate
+{
 
-    @IBOutlet var sceneView: ARSCNView!
-    
-    override func viewDidLoad() {
+    @IBOutlet var sceneView : ARSCNView!
+    private var mauerNode_ = SCNNode()
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         sceneView.delegate = self
         sceneView.showsStatistics = true
 
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene( named: "art.scnassets/MauerEinfach scaling copy.scn" )!
+        if let node = scene.rootNode.childNode( withName: "MauerEinfach", recursively: true )
+        {
+            mauerNode_ = node
+        }
+
+        mauerNode_.position = SCNVector3( 0, -2.5, -2.0 )
+
         sceneView.scene = scene
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+
+    override func viewWillAppear( _ animated : Bool )
+    {
+        super.viewWillAppear( animated )
         let configuration = ARWorldTrackingConfiguration()
-        sceneView.session.run(configuration)
+        sceneView.session.run( configuration )
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+
+    override func viewWillDisappear( _ animated : Bool )
+    {
+        super.viewWillDisappear( animated )
         sceneView.session.pause()
     }
-    
-    override func didReceiveMemoryWarning() {
+
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
     // MARK: - ARSCNViewDelegate
-    
+
 /*
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
@@ -49,18 +62,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
+
+    func session( _ session : ARSession, didFailWithError error : Error )
+    {
         // Present an error message to the user
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
+
+    func sessionWasInterrupted( _ session : ARSession )
+    {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
 
     }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
+
+    func sessionInterruptionEnded( _ session : ARSession )
+    {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+
     }
 }
