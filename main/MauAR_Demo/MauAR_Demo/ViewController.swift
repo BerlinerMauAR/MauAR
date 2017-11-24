23189f604a791c5ace8d41cbf9be1991e93a4c97
Peter Kolski <peter@bildpeter.de>
2017-11-24 18:44:10 +0100

    CleanUp

--- main/MauAR_Demo/MauAR_Demo/ViewController.swift
+++ main/MauAR_Demo/MauAR_Demo/ViewController.swift
@@ -44,40 +44,6 @@ class ViewController: UIViewController, ARSCNViewDelegate
         sceneView.session.pause()
     }
 
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-
-    func session( _ session : ARSession, didFailWithError error : Error )
-    {
-        // Present an error message to the user
-    }
-
-    func sessionWasInterrupted( _ session : ARSession )
-    {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-
-    }
-
-    func sessionInterruptionEnded( _ session : ARSession )
-    {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-
-    }
-
     func renderer( _ renderer : SCNSceneRenderer, didAdd node : SCNNode, for anchor : ARAnchor )
     {
         if !isWallCreated_
